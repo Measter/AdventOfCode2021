507ae89279ae82eeef9361437d25c673a4cfa2da
Stuart Haidon <serayen.sh@gmail.com>
2021-12-13 16:58:00 +0000

    Day 13: More hashing, more speed!

--- src/days/day13.rs
+++ src/days/day13.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use aoc_lib::{Bench, BenchResult, Day, NoError, ParseResult};
 
 //11:32
@@ -43,7 +45,7 @@ fn run_parse(input: &str, b: Bench) -> BenchResult {
     b.bench(|| Ok::<_, NoError>(ParseResult(PaperInstructions::parse(input))))
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 struct Point {
     x: usize,
     y: usize,
@@ -56,6 +58,20 @@ impl Point {
         Point {
             x: x.trim().parse().unwrap(),
             y: y.trim().parse().unwrap(),
+        }
+    }
+
+    fn reflect(self, instruction: Instruction) -> Self {
+        match instruction {
+            Instruction::Y(y) if self.y > y => {
+                let y = 2 * y - self.y;
+                Self { y, ..self }
+            }
+            Instruction::X(x) if self.x > x => {
+                let x = 2 * x - self.x;
+                Self { x, ..self }
+            }
+            _ => self,
         }
     }
 }
@@ -84,32 +100,27 @@ impl Instruction {
 
 #[derive(Debug, Clone)]
 struct PaperInstructions {
-    dot_field: Vec<bool>,
-    width: usize,
+    dot_field: HashSet<Point>,
+    buffer: HashSet<Point>,
     folded_width: usize,
     folded_height: usize,
 }
 
 impl PaperInstructions {
-    fn parse(input: &str) -> (PaperInstructions, Vec<Instruction>) {
+    fn parse(input: &str) -> (Self, Vec<Instruction>) {
         let (point_str, instr_str) = input.split_once("\n\n").expect("Segment break not found");
 
-        let points: Vec<_> = point_str.trim().lines().map(Point::parse).collect();
+        let points: HashSet<_> = point_str.trim().lines().map(Point::parse).collect();
         let (width, height) = points
             .iter()
             .fold((0, 0), |(x, y), p| (x.max(p.x + 1), y.max(p.y + 1)));
-
-        let mut dot_field = vec![false; width * height];
-        points
-            .into_iter()
-            .for_each(|p| dot_field[p.y * width + p.x] = true);
 
         let instrs = instr_str.trim().lines().map(Instruction::parse).collect();
 
         (
             Self {
-                dot_field,
-                width,
+                dot_field: points,
+                buffer: HashSet::new(),
                 folded_height: height,
                 folded_width: width,
             },
@@ -119,13 +130,10 @@ impl PaperInstructions {
 
     fn print_paper(&self) -> String {
         let mut output = String::new();
-        for row in self
-            .dot_field
-            .chunks_exact(self.width)
-            .take(self.folded_height)
-        {
-            for tile in &row[..self.folded_width] {
-                if *tile {
+
+        for y in 0..self.folded_height {
+            for x in 0..self.folded_width {
+                if self.dot_field.contains(&Point { x, y }) {
                     output.push('#');
                 } else {
                     output.push(' ');
@@ -138,46 +146,22 @@ impl PaperInstructions {
     }
 
     fn make_fold(&mut self, instruction: Instruction) {
-        match instruction {
-            Instruction::Y(y) => {
-                let row_start = y * self.width;
-                let dot_field = &mut self.dot_field[..self.folded_height * self.width];
-                let (top, bottom) = dot_field.split_at_mut(row_start);
-
-                top.chunks_exact_mut(self.width)
-                    .rev()
-                    .zip(bottom[self.width..].chunks_exact(self.width))
-                    .for_each(|(top, bottom)| {
-                        top[..self.folded_width]
-                            .iter_mut()
-                            .zip(bottom)
-                            .for_each(|(t, &b)| *t |= b);
-                    });
-
-                self.folded_height = y;
-            }
-            Instruction::X(x) => {
-                for row in
-                    self.dot_field[..self.folded_height * self.width].chunks_exact_mut(self.width)
-                {
-                    let (left, right) = row.split_at_mut(x);
-
-                    left.iter_mut()
-                        .rev()
-                        .zip(&right[1..])
-                        .for_each(|(l, &r)| *l |= r);
-                }
-
-                self.folded_width = x;
-            }
+        self.buffer.clear();
+        for point in self.dot_field.drain() {
+            let mirrored = point.reflect(instruction);
+            self.buffer.insert(mirrored);
         }
+
+        match instruction {
+            Instruction::Y(y) => self.folded_height = y,
+            Instruction::X(x) => self.folded_width = x,
+        }
+
+        std::mem::swap(&mut self.buffer, &mut self.dot_field);
     }
 
     fn count_dots(&self) -> usize {
-        self.dot_field[..self.folded_height * self.width]
-            .chunks_exact(self.width)
-            .map(|row| row[..self.folded_width].iter().filter(|p| **p).count())
-            .sum()
+        self.dot_field.len()
     }
 }
 
